7b5ff46f24fa541455a2b5e8cd65f9f87447cd46
Kirpal Villa Villarreal <4kvlopera0010@gmail.com>
2021-11-05 07:45:26 -0500

    actualizo el proyecto

--- models/ServidorModelo.js
+++ models/ServidorModelo.js
@@ -10,7 +10,7 @@ class ServidorModelo {
     constructor() {
 
         this.app = express();
-        //this.despertarBD();
+        this.despertarBD();
         this.llamarAuxiliares();
         this.enrutarPeticiones();
 
